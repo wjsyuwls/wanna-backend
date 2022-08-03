bd3b0841e57f33b7ff155b70b8e0a9fcc9bfee49
wjsyuwls <am2ch1f@gmail.com>
2022-08-03 17:40:00 +0900

    🚧 클레이튼 컨트랙트 연결

--- routes/vote.js
+++ routes/vote.js
@@ -9,4 +9,18 @@ const db = mysql.createConnection({
   database: "wanna",
 });
 
+//klaytn contract 연결
+var Caver = require("caver-js");
+var cav = new Caver("https://api.baobab.klaytn.net:8651");
+var product_contract = require("../build/vote.json");
+var smartcontract = new cav.klay.Contract(
+  product_contract.abi,
+  "0xd9145CCE52D386f254917e481eB44e9943F39138"
+);
+var account = cav.klay.accounts.createWithAccountKey(
+  process.env.address,
+  process.env.privatekey
+);
+cav.klay.accounts.wallet.add(account);
+
 module.exports = router;
