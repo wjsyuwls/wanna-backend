74141ab139f5ece8f5b294ec50842fbb8bce9eb6
MinKi1506 <93629619+MinKi1506@users.noreply.github.com>
2022-08-04 18:35:51 +0900

    리뷰 조회

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.use(express.urlencoded({ extended: false }));
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+});
+
 const api = require("./routes/api");
 app.use("/api", api);
 
@@ -22,4 +31,19 @@ app.use("/api/review", review);
 const port = 3001;
 app.listen(port, () => {
   console.log(`running on port ${port}`);
+});
+
+app.get("/api/getReview", (req, res) => {
+  db.query("select * from review", [], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send("SQL ERROR");
+    } else {
+      console.log(result);
+      if (result.length > 0) {
+        console.log("보내는 내용: " + result);
+        res.send(result);
+      }
+    }
+  });
 });
