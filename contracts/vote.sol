c5b61e01b02f954b59c2253736751af9482a5550
wjsyuwls <am2ch1f@gmail.com>
2022-08-07 23:25:55 +0900

    📝 검증된 리뷰

--- contracts/vote.sol
+++ contracts/vote.sol
@@ -3,7 +3,6 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Review {
-
     struct voting {
         address[] agree;
         address[] disagree;
@@ -11,44 +10,95 @@ contract Review {
     }
 
     struct review {
-        string place;
-        string title; 
-        string content;
         address writer;
+        string place;
+        string title;
+        string content;
+        string img;
+        uint256 score;
     }
 
-    mapping(uint => voting) public votings;
-    mapping(uint => review) public reviews;
+    mapping(uint256 => voting) public votings;
+    mapping(string => uint256[]) public place_reviews;
+    mapping(uint256 => review) public reviews;
 
-    function add_voting(uint _review_id, bool _a_d, address _voter) public{
-        if (_a_d){
+    function add_voting(
+        uint256 _review_id,
+        bool _a_d,
+        address _voter
+    ) public {
+        if (_a_d) {
             votings[_review_id].agree.push(_voter);
-        }else{
+        } else {
             votings[_review_id].disagree.push(_voter);
         }
         votings[_review_id].count++;
     }
-    function view_voting(uint _review_id) public view returns (address[] memory, address[] memory, uint8){
+
+    function view_voting(uint256 _review_id)
+        public
+        view
+        returns (
+            address[] memory,
+            address[] memory,
+            uint8
+        )
+    {
         address[] memory Agree = votings[_review_id].agree;
         address[] memory Disagree = votings[_review_id].disagree;
         uint8 count = votings[_review_id].count;
-        return ( Agree, Disagree, count);
+        return (Agree, Disagree, count);
     }
 
-    function add_review(uint _review_id, string memory _place, string memory _title, string memory _content, address _writer) public {
+    function add_place_review(string memory _place, uint256 _review_id) public {
+        place_reviews[_place].push(_review_id);
+    }
+
+    function add_review(
+        uint256 _review_id,
+        address _writer,
+        string memory _place,
+        string memory _title,
+        string memory _content,
+        string memory _img,
+        uint256 _score
+    ) public {
+        reviews[_review_id].writer = _writer;
         reviews[_review_id].place = _place;
         reviews[_review_id].title = _title;
         reviews[_review_id].content = _content;
-        reviews[_review_id].writer = _writer;
+        reviews[_review_id].img = _img;
+        reviews[_review_id].score = _score;
+        add_place_review(_place, _review_id);
     }
 
-    function view_review(uint _review_id) public view returns (string memory, string memory, string memory, address){
+    function view_place_review(string memory _place)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory place_review = place_reviews[_place];
+        return place_review;
+    }
+
+    function view_review(uint256 _review_id)
+        public
+        view
+        returns (
+            address,
+            string memory,
+            string memory,
+            string memory,
+            string memory,
+            uint256
+        )
+    {
+        address writer = reviews[_review_id].writer;
         string memory place = reviews[_review_id].place;
         string memory title = reviews[_review_id].title;
         string memory content = reviews[_review_id].content;
-        address writer = reviews[_review_id].writer;
-        return ( place, title, content, writer );
-
+        string memory img = reviews[_review_id].img;
+        uint256 score = reviews[_review_id].score;
+        return (writer, place, title, content, img, score);
     }
 }
-
--- routes/review.js
+++ routes/review.js
@@ -11,6 +11,39 @@ const db = mysql.createConnection({
   database: process.env.database,
 });
 
+// connect klaytn contract
+var Caver = require("caver-js");
+var cav = new Caver("https://api.baobab.klaytn.net:8651");
+var product_contract = require("../build/vote.json");
+var smartcontract = new cav.klay.Contract(
+  product_contract.abi,
+  process.env.contract_account
+);
+
+// view vote scf
+const view_voting = async (id) => {
+  let Agree, Disagree, Count;
+  await smartcontract.methods
+    .view_voting(id)
+    .call()
+    .then((receipt) => {
+      Agree = receipt["0"];
+      Disagree = receipt["1"];
+      Count = receipt["2"];
+    });
+  return { Agree, Disagree, Count };
+};
+
+async function view_place_review(place) {
+  const receipt = await smartcontract.methods.view_place_review(place).call();
+  return receipt;
+}
+
+async function view_review(id) {
+  const receipt = smartcontract.methods.view_review(id).call();
+  return receipt;
+}
+
 router.get("/", (req, res) => {
   db.query("select * from review", [], (err, result) => {
     if (err) {
@@ -18,7 +51,19 @@ router.get("/", (req, res) => {
       res.send("SQL ERROR");
     } else {
       if (result.length > 0) {
-        res.send(result);
+        let place = [];
+        result.map((data) => {
+          console.log(data);
+          view_voting(data.id).then(({ Agree, Disagree, Count }) => {
+            place.push({ ...data, Agree, Disagree, Count });
+            console.log({ ...data, Agree, Disagree, Count });
+          });
+        });
+
+        setTimeout(() => {
+          console.log(place);
+          res.send(place);
+        }, 100);
       }
     }
   });
@@ -28,14 +73,26 @@ router.post("/add", (req, res) => {
   const place_name = req.body.place_name;
   const title = req.body.title;
   const nickname = req.body.nickname;
+  const address = req.body.address;
   const score = req.body.score;
   const img = req.body.img;
   const content = req.body.content;
-  const _date = moment().add(7, "days").format("YYYY/MM/DD HH:mm:ss");
+  const _date = moment().format("YYYY/MM/DD HH:mm:ss");
+  const deadline = moment().add(7, "days").format("YYYY/MM/DD HH:mm:ss");
 
   db.query(
-    "insert into review (place_name, title, nickname, score, img, content, _date) values (?, ?, ?, ?, ?, ?, ?)",
-    [place_name, title, nickname, score, img, content, _date],
+    "insert into review (place_name, title, nickname, address, score, img, content, _date, deadline) values (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      place_name,
+      title,
+      nickname,
+      address,
+      score,
+      img,
+      content,
+      _date,
+      deadline,
+    ],
     (err, result) => {
       if (err) {
         console.log(err);
@@ -45,6 +102,39 @@ router.post("/add", (req, res) => {
       }
     }
   );
+});
+
+router.get("/verify/:place", async (req, res) => {
+  const receipt = await view_place_review(req.params.place);
+  let reviews = [];
+  receipt.map(async (id) => {
+    db.query("select * from review where id = ?", [id], async (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send("SQL ERROR");
+      } else {
+        if (result.length > 0) {
+          const sc_review = await view_review(id);
+          console.log(sc_review);
+          const review = {
+            id: result[0].id,
+            title: sc_review["2"],
+            nickname: result[0].nickname,
+            content: sc_review["3"],
+            img: sc_review["4"],
+            score: sc_review["5"],
+            like: result[0]._like,
+            date: result[0]._date,
+          };
+          reviews.push(review);
+        }
+      }
+    });
+  });
+
+  setTimeout(() => {
+    res.send(reviews);
+  }, 100);
 });
 
 module.exports = router;
